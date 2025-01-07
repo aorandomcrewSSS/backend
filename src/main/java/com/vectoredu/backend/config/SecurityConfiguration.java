220dfeb63e3656aca18a6e1faa33afcb845611c6
aorandomcrewSSS <danbogomolovhr@gmai.com>
2025-01-07 19:09:04 +0300

    CORS fix2

--- src/main/java/com/vectoredu/backend/config/SecurityConfiguration.java
+++ src/main/java/com/vectoredu/backend/config/SecurityConfiguration.java
@@ -51,8 +51,8 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://app-backend.com", "http://localhost:8080", "http://localhost:3000"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedOrigins(List.of("https://app-backend.com", "http://localhost:8080", "http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
